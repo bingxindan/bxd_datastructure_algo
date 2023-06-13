ae2c3f586fe1db86cc13dab20d27b77b35ac09f3
zhangming03 <zhangming03@qianxin.com>
2023-06-14 00:48:01 +0800

    tree

---
+++ 5_Tree/17_NumTrees/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	response := numTrees(3)
+	fmt.Printf("resp: %+v\n", response)
+}
+
+func numTrees(n int) int {
+	G := make([]int, n+1)
+	G[0], G[1] = 1, 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			G[i] += G[j-1] * G[i-j]
+		}
+	}
+	return G[n]
+}
