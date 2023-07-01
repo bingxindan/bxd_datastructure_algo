eb793cf8cb9b0e7ae39c3fda5a4ca8584d7c36e3
zhangming03 <zhangming03@qianxin.com>
2023-07-01 14:17:13 +0800

    bsf

--- 8_Search/14_BFS/main.go
+++ 8_Search/14_BFS/main.go
@@ -1,10 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 
 }
 
 func numWays(n int, relation [][]int, k int) int {
 	edges := make([][]int, n)
-	for _, r := range
+	for _, r := range relation {
+		fmt.Println(edges, r)
+	}
+	return 0
 }
